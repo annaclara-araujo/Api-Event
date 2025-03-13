9a7774f94f97bdc4aeefac30a4d38c7fd885e136
Anna Clara <annaclaraaraujo229@gmail.com>
2025-03-13 16:57:26 -0300

    feat: Iniciando API Event

---
+++ Domains/ComentarioEvento.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Event.Domains
+{
+    [Table("ComentarioEvento")]
+    [Index(nameof(Exibe), IsUnique = true)]
+
+    public class ComentarioEvento
+    {
+        [Key]
+        public  Guid ComentarioEventoId { get; set; }
+
+        [Column(TypeName = "TEXT")]
+        [Required(ErrorMessage = "A descrição é obrigatória")]
+        public string? Comentario { get; set; }
+
+
+        [Column(TypeName = "BIT")]
+        [Required(ErrorMessage = "A resposta é necessária")]
+        public bool? Exibe { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Usuario obrigatorio")]
+        public Guid UsuarioId { get; set; }
+
+        [ForeignKey("Usuario")]
+        public Usuario? Usuario { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Evento obrigatorio")]
+        public Guid EventoId { get; set; }
+
+        [ForeignKey("Evento")]
+        public Evento? Evento { get; set; }
+    }
+}
---
+++ Domains/Presenca.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Event.Domains
+{
+
+    [Table("Presenca")]
+    [Index(nameof(Situacao), IsUnique = true)]
+
+    public class Presenca
+    {
+        [Key]
+
+        public Guid PresencaId { get; set; }
+
+        
+        [Column(TypeName = "BIT")]
+        [Required(ErrorMessage = "Campo obrigatorio!")]
+        public bool? Situacao { get; set; }
+
+        
+        
+        [Required(ErrorMessage = "Usuario obrigatorio")]
+        public Guid UsuarioId { get; set; } 
+
+        [ForeignKey("Usuario")]
+        public Usuario? Usuario { get; set; }
+
+
+
+
+        [Required(ErrorMessage = "Evento obrigatorio")]
+        public Guid EventoId { get; set; }
+
+        [ForeignKey("Evento")]
+        public Evento? Evento { get; set; }
+
+
+
+
+
+
+    }
+}
